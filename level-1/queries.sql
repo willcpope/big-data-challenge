dc5886fe07f6cbc1908b6d259c7944196ed1de72
Will Pope <57363781+willcpope@users.noreply.github.com>
2020-05-19 11:05:18 -0400

    level 1 upload

---
+++ level-1/queries.sql
@@ -0,0 +1,19 @@
+SELECT * FROM review_id_table
+LIMIT 10;
+
+SELECT COUNT(*) FROM review_id_table;
+
+SELECT * FROM products
+LIMIT 10;
+
+SELECT COUNT(*) FROM products;
+
+SELECT * FROM customers
+LIMIT 10;
+
+SELECT COUNT(*) FROM customers;
+
+SELECT * FROM vine_table
+LIMIT 10;
+
+SELECT COUNT(*) FROM vine_table;
